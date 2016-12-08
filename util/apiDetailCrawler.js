af581a4efd5b4985cd23a10a35d4cbc79d71a61c
Nicolas Ferreira <nickrfer@gmail.com>
2016-12-07 22:17:40 -0200

    Separating files.

--- util/apiDetailCrawler.js
+++ util/apiDetailCrawler.js
@@ -2,5 +2,5 @@ var cheerio = require('cheerio');
 
 exports.crawl = function(html, javaVersion) {
     var $ = cheerio.load(html);
-    console.log($("body:contains('" + javaVersion + "')"));
+    console.log(html.includes(javaVersion));
 }
