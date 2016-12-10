c73e75b69341a2cf4110a858636d27a04911b16a
Nicolas Ferreira <nickrfer@gmail.com>
2016-12-10 12:25:11 -0200

    Adding type info class.

--- javadocs-json/util/apiDetailCrawler.js
+++ javadocs-json/util/apiDetailCrawler.js
@@ -1,5 +1,9 @@
 var cheerio = require('cheerio');
 
+function TypeInfo(name) {
+    this.name = name;
+}
+
 exports.crawl = function(html, javaVersion) {
     var $ = cheerio.load(html);
     if ($("body:contains('" + javaVersion + "')").length > 0) {
